b64f091e11fb57c89b15c37a3dc727c3d54eeb67
jinzhunan <47770525+jinzhunan@users.noreply.github.com>
2021-05-08 21:42:52 -0400

    second commit

--- config/plugins.js
+++ config/plugins.js
@@ -7,15 +7,18 @@ module.exports=({ env })=>   ({
         api_secret: 'h3urE5uA-V-7sffed06oBL1qsH8'
       }
     },
+    
     email: {
-        provider: "nodemailer-v3",
-        providerOptions: {},
-        settings: {
-            host: 'smtp.gmail.com',
-            port: 587,
-            username: 'nanjinzhu202012@gmail.com',
-            password: 'xq_b6!r?237wjqW',
-            secure: false
-        }
-    },
+        provider: 'nodemailer',
+        providerOptions: {
+          host: 'smtp.gmail.com',
+          port: 587,
+          auth: {
+            user: 'nanjinzhu202012@gmail.com',
+            pass: 'xq_b6!r?237wjqW',
+          },
+          // ... any custom nodemailer options
+        },
+        settings: {},
+      },
   });
