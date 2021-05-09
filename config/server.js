027d116515b37ddb57c13e0b5e02e6f9a5d6bc87
jinzhunan <47770525+jinzhunan@users.noreply.github.com>
2021-05-09 07:15:51 -0400

    url change

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  // url: "https://jinzhu-game.herokuapp.com",
+  url: "https://jinzhu.herokuapp.com",
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '54161cfdf412334a3045a2e31b9c517e'),
